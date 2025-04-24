1acc25a3f44225eb783e4bcf1383adb3a9b5a8c1
AlessandroStefanucci <aless.stefanucci@gmail.com>
2025-04-24 11:11:48 +0200

    Aggiunta immagini vittoria/sconfitta

--- app/src/main/java/com/example/poggiani_rolldice/ThirdActivity.kt
+++ app/src/main/java/com/example/poggiani_rolldice/ThirdActivity.kt
@@ -2,6 +2,7 @@ package com.example.poggiani_rolldice
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -25,12 +26,17 @@ class ThirdActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textViewActivityThird)
 
+        val imageViewThird = findViewById<ImageView>(R.id.imageViewActivityThird)
+
+
         if (numeroEstratto == numeroInserito) {
             textView.text = "Complimenti! \nHai indovinato... \nEra proprio $numeroEstratto"
             Log.d(TAG, "Esito: indovinato")
+            imageViewThird.setImageResource(R.drawable.win)
         } else {
             textView.text = "Peccato! \nNon hai indovinato... \nHai scelto $numeroInserito \nma il numero era $numeroEstratto"
             Log.d(TAG, "Esito: non indovinato")
+            imageViewThird.setImageResource(R.drawable.lose)
         }
     }
 }
